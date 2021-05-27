15bdd6cc8c177feb97b53aa5b1c4fae2f6077cf6
cestwc <80936226+cestwc@users.noreply.github.com>
2021-05-28 01:17:43 +0800

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,20 @@
+import torch
+from utils import highlight, erase
+
+def summarize(src, trg, highlighter, writer, tokenizer):
+	if isinstance(src, str):
+		src = tokenizer(src, max_length=1024, return_tensors="pt")
+		trg = tokenizer(trg, max_length=1024, return_tensors="pt")
+		
+	src = src.to(writer.device)
+	trg = trg.to(writer.device)
+	
+	h_mask = highlight(src, trg)
+	
+	outputs = highlighter(**inputs, labels=h_mask)
+	logits = outputs.logits
+	
+	src_erased = erase(src, logits.argmax(2)).to(writer.device)
+	summary_ids = writer.generate(src_erased, num_beams=4, max_length=100, early_stopping=True)
+	
+	return [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
