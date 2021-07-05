1e956b489b397331bd82308132bb82010fc9816d
cestwc <80936226+cestwc@users.noreply.github.com>
2021-07-06 01:04:47 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -12,7 +12,7 @@ def tokenClassificationTrainStep(model, optimizer, clip, src, labels, attention_
 	else:
 		logits = torch.cat((model(src[:, :512], attention_mask = attention_mask[:, :512]).logits, model(src[:, 512:], attention_mask = attention_mask[:, 512:]).logits), dim = 1)
 	
-	counts = torch.unique(labels.masked_select(attention_mask), return_counts = True)[1] if attention_mask is not None else torch.unique(labels), return_counts = True)[1]
+	counts = torch.unique(labels.masked_select(attention_mask), return_counts = True)[1] if attention_mask is not None else torch.unique(labels, return_counts = True)[1]
 	criterion = torch.nn.CrossEntropyLoss(weight = torch.log(torch.max(counts) / counts) + 1)
 	
 	if attention_mask is not None:
@@ -44,7 +44,7 @@ def tokenClassificationEvalStep(model, src, labels, attention_mask = None):
 	else:
 		logits = torch.cat((model(src[:, :512], attention_mask = attention_mask[:, :512]).logits, model(src[:, 512:], attention_mask = attention_mask[:, 512:]).logits), dim = 1)
 	
-	counts = torch.unique(labels.masked_select(attention_mask), return_counts = True)[1] if attention_mask is not None else torch.unique(labels), return_counts = True)[1]
+	counts = torch.unique(labels.masked_select(attention_mask), return_counts = True)[1] if attention_mask is not None else torch.unique(labels, return_counts = True)[1]
 	criterion = torch.nn.CrossEntropyLoss(weight = torch.log(torch.max(counts) / counts) + 1)
 	
 	if attention_mask is not None:
