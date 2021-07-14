9645126c61dab41e592bcd041ad6f7d002a36e84
cestwc <80936226+cestwc@users.noreply.github.com>
2021-07-14 10:09:29 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,8 +19,10 @@ def erase(article_tensor, highlight_mask):
 	return torch.nn.utils.rnn.pad_sequence(l, batch_first=True, padding_value=1).long()
 
 def binary_metric(preds, y, attention_mask):
-	rounded_preds = torch.sigmoid(preds) > 0.5 * attention_mask
-	print(rounded_preds.shape, y.shape)
+	preds = torch.sigmoid(preds).squeeze(-1) * attention_mask
+
+	rounded_preds = preds > 0.5
+
 	t = rounded_preds == y
 	p = rounded_preds == 1
 	tp = torch.sum(torch.logical_and(t, p).float(), dim = 1)
